41467200905aa24e6cef1c36aa22c32e9027b9cd
John Tan <tantai.john@gmail.com>
2015-03-24 16:11:21 -0700

    fixed bug with autoscrolling

--- client/_testserver/server.js
+++ client/_testserver/server.js
@@ -1,6 +1,35 @@
 var io = require('socket.io');
 var Server = require('socket.io');
+var spawn = require('child_process').spawn;
+
 var io = new Server(8000);
-setInterval(function(){
-  io.emit('data', Math.random().toString());
-}, 100)
+// setInterval(function(){
+//   io.emit('output', Math.random().toString());
+// }, 100)
+
+function emitOutput(data){
+  io.emit('output', String(data));
+};
+
+io.on('connection', function(socket){
+  socket.on('input', function(data){
+    try {
+      var commands = data.split(' ');
+      var command = commands[0];
+      var args = commands.slice(1);
+      console.log(command, args);
+      var proc = spawn(data, args);
+      proc.on('error', function(err){
+        console.log('invalid command', err);
+        emitOutput('invalid command : ' + data);
+      })
+      proc.on('close', function (code) {
+      });
+      proc.stdout.on('data', emitOutput);
+      proc.stderr.on('data', emitOutput);
+    } catch (e){
+      console.log('error executing');
+      emitOutput('Error executing : ' + e);
+    }
+  })
+})
