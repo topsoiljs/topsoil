c7bfc6043fec7f6309c051e89c57aaf6f38308e1
asakurasol <mamamia1734@gmail.com>
2015-03-28 10:43:23 -0700

    temp save

--- deploy/server/server.js
+++ deploy/server/server.js
@@ -1,37 +1,39 @@
-/// <reference path="../typings/hapi/hapi.d.ts"/>
+/// <reference path="../../typings/hapi/hapi.d.ts"/>
 var Server = require('socket.io');
 var spawn = require('child_process').spawn;
-var io = new Server(8000);
-// setInterval(function(){
-//   io.emit('output', Math.random().toString());
-// }, 100)
-function emitOutput(data) {
-    io.emit('output', String(data));
-}
-;
-io.on('connection', function (socket) {
-    socket.on('input', function (data) {
-        data = String(data);
-        try {
-            var commands = data.split(' ');
-            var command = commands[0];
-            var args = commands.slice(1);
-            args = args.map(function (el) {
-                return el;
-            });
-            var proc = spawn(command, args);
-            proc.on('error', function (err) {
-                console.log('invalid command', err);
-                emitOutput('invalid command : ' + data);
-            });
-            proc.on('close', function (code) {
-            });
-            proc.stdout.on('data', emitOutput);
-            proc.stderr.on('data', emitOutput);
-        }
-        catch (e) {
-            console.log('error executing');
-            emitOutput('Error executing : ' + e);
-        }
-    });
-});
+//var io = new Server(8000);
+//// setInterval(function(){
+////   io.emit('output', Math.random().toString());
+//// }, 100)
+//
+//function emitOutput(data){
+//  io.emit('output', String(data));
+//};
+//
+//io.on('connection', function(socket){
+//  socket.on('input', function(data){
+//    data = String(data);
+//    try {
+//      var commands = data.split(' ');
+//      var command = commands[0];
+//      var args = commands.slice(1);
+//      args = args.map(function(el){return el});
+//      var proc = spawn(command, args);
+//
+//      proc.on('error', function(err){
+//        console.log('invalid command', err);
+//        emitOutput('invalid command : ' + data);
+//      })
+//
+//      proc.on('close', function (code) {
+//      });
+//
+//      proc.stdout.on('data', emitOutput);
+//
+//      proc.stderr.on('data', emitOutput);
+//    } catch (e){
+//      console.log('error executing');
+//      emitOutput('Error executing : ' + e);
+//    }
+//  })
+//}) 
