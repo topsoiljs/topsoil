4ec53e6fee0fe549d75e13bd235e2011cf4f0d15
John Tan <tantai.john@gmail.com>
2015-03-29 19:50:33 -0700

    fixed bugs

--- deploy/client/scripts/magic/magic.js
+++ deploy/client/scripts/magic/magic.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 var Magic = function(){
   this.views = {};
 };
@@ -13,14 +15,14 @@ Magic.prototype.callCommand = function(command){
 Magic.prototype.searchCommand = function(terms){
   var results = [];
   // Brute force search for now
-  this.views.forEach(function(view){
+  _.each(this.views, function(view){
     view.commands.forEach(function(command){
-      if(command.name.indexOf(term) > -1){
+      if(command.name.indexOf(terms) > -1){
         results.push(command);
         return;
       };
       for(var i=0;i<command.tags.length;i++){
-        if(command.tags[i].indexOf(term) > -1){
+        if(command.tags[i].indexOf(terms) > -1){
           results.push(command);
           return;
         }
@@ -31,5 +33,22 @@ Magic.prototype.searchCommand = function(terms){
   return results;
 };
 
+// var test = new Magic();
 
+// test.registerView({
+//   name: 'filesystem',
+//   commands: [
+//     {
+//       name: 'getFiles',
+//       description: 'lists files in directory',
+//       args: 'directory',
+//       tags: ['show files', 'list files', 'display files'],
+//       categories: ['read'],
+//       method: function(dir){
+//       }
+//     }
+//   ],
+//   category: 'filesystem'
+// });
 
+// console.log(test.searchCommand('show'));
