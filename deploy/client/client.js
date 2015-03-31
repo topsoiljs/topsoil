f2f42428108db53127a1cdd96cbee9966a864780
Brian Zindler <zindlerb@gmail.com>
2015-03-31 13:24:39 -0700

    finished component

---
+++ deploy/client/client.js
@@ -0,0 +1,126 @@
+var EventBus = function() {
+  
+  var events = {};
+
+  var api = {
+    register: function(view, listener) {
+      events[view] = listener;
+    },
+    emit: function(view, data) {
+      if(events[view]) {
+        events[view](data);
+      }
+    }
+  }
+
+  return api;
+}
+
+var eventBus = EventBus();
+  
+
+
+/*
+START DUMMY DATA
+*/
+
+function io(path) {
+  return {
+    emit: function() {
+
+    },
+    on: function(dummy, func) {
+      func({data: ["deploy",
+                   "client",
+                   "tests",
+                   "secret"]});
+    }
+  } 
+}
+
+var magic = {
+  registerView: function() {
+
+  }
+}
+
+
+
+/*
+END DUMMY DATA
+*/
+
+function ViewStore() {
+  var state = {files: []};
+  var socket = io('/');
+
+  var methods = {
+    getFiles: function(dir){
+          var UID = Math.random();
+          socket.emit('fs.ls', {
+            dir: dir,
+            uid: UID
+          });
+          socket.on(UID, function(data){
+            //data.err
+            //data.data
+            state.files = data.data;
+            eventBus.emit('filesystem');
+          })
+    },
+    getState: function() {
+      return state;
+    }
+  }
+
+  return methods;
+}
+
+var viewStore = ViewStore();
+
+var FilesystemComponent = React.createClass({displayName: "FilesystemComponent",
+  getInitialState: function() {
+    return viewStore.getState();
+  },
+  componentDidMount: function() {
+    eventBus.register("filesystem", function() {
+      console.log(this);
+      this.setState(viewStore.getState());
+    }.bind(this));
+
+    document.getElementById("show").onclick = function(e) {
+      e.preventDefault();
+      viewStore.getFiles();
+    }
+  },
+  render: function() {
+    
+    var fileText = this.state.files.map(function(filename) {
+      return (React.createElement("p", null, " ", filename, " "))
+    });
+
+    return (React.createElement("div", null, 
+       "Filesystem", 
+       fileText, 
+       React.createElement("a", {id: "show", href: ""}, "show files")
+    ));
+  }
+});
+
+React.render(React.createElement(FilesystemComponent, null), document.getElementById('input'));
+
+magic.registerView({
+  name: 'filesystem',
+  commands: [{
+    getFiles: { 
+      description: 'lists files in directory',
+      args: 'directory',
+      tags: ['show files', 'list files', 'display files'],
+      categories: ['read'],
+      method: viewStore["getFiles"]
+    }
+  }],
+  category: 'filesystem',
+  component: FilesystemComponent
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,13 @@ var merge = require('merge2');
 var jade = require('gulp-jade');
 var stylus = require('gulp-stylus');
 var watch = require('gulp-watch');
- 
+var react = require('gulp-react');
+var concat = require('gulp-concat'); 
 
 gulp.task('jade', function () {
   var YOUR_LOCALS = {};
  
-  gulp.src('./deploy/templates/*.jade')
+  gulp.src('./client/templates/*.jade')
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
@@ -21,11 +22,11 @@ gulp.task('jade', function () {
 });
 
 gulp.task('stylus', function() {
-  gulp.src('./deploy/stylus_styles/*.styl')
+  gulp.src('./client/stylus_styles/*.styl')
     .pipe(stylus({
       compress: true
     }))
-    .pipe(gulp.dest('./deploy/client/styles'));
+    .pipe(gulp.dest('./deploy/client'));
 });
 
 gulp.task('ts', function () {
@@ -50,10 +51,20 @@ gulp.task('ts', function () {
       ]);
 });
 
+gulp.task('jsx', function(){
+   return gulp.src('client/**/*.jsx')
+              .pipe(react())
+              .pipe(concat('client.js'))
+              .pipe(gulp.dest('deploy/client'));
+})
+
 gulp.task('tsw', function () {
-  gulp.watch("**/*.ts", ["ts"])
+  gulp.watch("**/*.ts", ["ts"]);
 });
 
+gulp.task('jsx-w', function () {
+  gulp.watch("**/*.jsx", ["jsx"]);
+});
 
 gulp.task('build-all', ['jade', 'stylus', 'ts']);
 
