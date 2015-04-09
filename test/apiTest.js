e343b2a28ab656f0bbe9d32e23693e288d8d1932
asakurasol <mamamia1734@gmail.com>
2015-04-09 15:37:35 -0700

    add testing for git status

--- test/apiTest.js
+++ test/apiTest.js
@@ -163,3 +163,31 @@ describe("File System View APIs",function(){
     })
   }); 
 });
+
+
+describe("Git View APIs",function(){
+  it('should be able to call git status', function(done){
+    var client = io('http://localhost:8000/',{'force new connection':true});
+    var UID = Math.random();
+
+    client.on('connect', function(data){
+      client.emit('git.status', {
+        cmd: 'git',
+        args: ['-s'],
+        dir: currentDir,
+        uid: UID
+      });
+      client.on(UID, function(data){
+        assert.typeOf(data, 'object', 'receive an object back');
+        assert.isTrue(data.hasOwnProperty('err')&&data.hasOwnProperty('data'), 'object has "err" and "data" properties');
+        assert.isTrue(data.data.hasOwnProperty('staged'));
+        assert.isTrue(data.data.hasOwnProperty('unstaged'));
+        assert.isTrue(data.data.hasOwnProperty('untracked'));
+        client.disconnect();
+        done();
+      });
+    })
+  });
+
+
+})
