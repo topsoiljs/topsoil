6fc485452937364ed2800b0847c9af144678cd13
asakurasol <mamamia1734@gmail.com>
2015-04-09 19:57:07 -0700

    add git add and reset to git api with testing

--- server-typescript/server/utility/utility.ts
+++ server-typescript/server/utility/utility.ts
@@ -140,4 +140,12 @@ utility.interleave = function(arr1: Array<any>, arr2: Array<any>) {
   }, []);
 }
 
+utility.identity = function(data){
+    if(!data){
+        console.log('no data passed');
+        return '';
+    }
+    return data;
+}
+
 module.exports = utility;
