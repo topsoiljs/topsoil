419aac48089c0dd465de9447d1a8401c06c9aa84
John Tan <tantai.john@gmail.com>
2015-04-10 16:57:52 -0700

    added example

--- server-typescript/server/streaming/streaming.ts
+++ server-typescript/server/streaming/streaming.ts
@@ -1,4 +1,5 @@
 var through = require('through2');
+var gulp = require('gulp');
 
 var createSocketStream = function(socket, id : string){
   return through(function(chunk, enc, cb){
@@ -6,3 +7,29 @@ var createSocketStream = function(socket, id : string){
     cb();
   })
 }
+
+var io = require('socket.io');
+var ioClient = require('socket.io-client');
+
+var convertToBuffer = function(){
+  return through.obj(function(chunk, enc, cb){
+    var buffed = new Buffer(chunk._contents);
+    cb(null, String(buffed));
+  })
+};
+var server = io(8001);
+
+server.on('connection', function(socket){
+  socket.on('newthing', function(data){
+    var iostream = createSocketStream(socket, data);
+    gulp.src('/Users/johntan/code/streamsPlay/nums.txt')
+      .pipe(convertToBuffer())
+      .pipe(iostream)
+  })
+})
+var client = ioClient('http://localhost:8001');
+var secret = "101";
+client.emit('newthing', secret);
+client.on(secret, function(data){
+  console.log(data);
+})
