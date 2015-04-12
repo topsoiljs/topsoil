043c32a98c8ed3454318712f2ad2411c1a36177e
John Tan <tantai.john@gmail.com>
2015-04-12 15:59:15 -0700

    initial refactor of API streams

--- server-typescript/app.ts
+++ server-typescript/app.ts
@@ -4,6 +4,9 @@
 var Hapi = require("hapi");
 var processManager = require('./server/processManager');
 var stateRoutes = require('./server/stateAPI/stateRoutes');
+var streaming = require('./server/streaming/streaming');
+var createInSocketStream = streaming.createInStream;
+var createOutSocketStream = streaming.createOutStream;
 //import validation = require('./server/Server');
 //// Create a server with a host and port
 var server = new Hapi.Server();
@@ -24,6 +27,12 @@ io.on('connection', function(socket){
       for(var namespace in api){
           for(var methodName in api[namespace]){
               socket.on(namespace + '.' + methodName, api[namespace][methodName](socket))
+              socket.on(namespace + '.' + methodName, function(opts){
+                var inStream = createInSocketStream(socket, opts.uid);
+                var outStream = createOutSocketStream(socket, opts.uid);
+                var commandStream = api[namespace][methodName](opts);
+                inStream.pipe(commandStream).pipe(outStream);
+              })
           }
       }
   }
