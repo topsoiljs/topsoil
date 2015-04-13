b739772eb1a67a26b71975048f95cc40bca28cf5
John Tan <tantai.john@gmail.com>
2015-04-13 15:21:53 -0700

    added comments explaining new API

--- client/streaming/streaming_client.js
+++ client/streaming/streaming_client.js
@@ -1,6 +1,43 @@
 window._globalSocket = io();
 
 var createNewStream = function(options){
+  /*
+    options should be of form
+    {
+      command: string : command you want to run,
+      opts: object : options object that will be passed to api on backend,
+      cb: function : callback (data) => void,
+      initialData: string : OPTIONAL data that will be passed through input pipe if chaining.
+      commands: array : OPTIONAL array of command objects for use with chaining command, SEE BELOW
+    }
+
+    cb will receive data of form
+    {
+      data: string : the payload
+    }
+  */
+
+  /*
+    For the 'chain' command,
+    specify the commands you want to chain, in order, with an array of this form
+    commands: [
+      {
+        name: 'fs.readFile',
+        opts: {
+          dir: '/Users/johntan/code/topsoil/client/magic/magicInput.jsx'
+        }
+      },
+      {
+        name: 'terminal.callCommand',
+        opts: {
+          cmd: 'grep',
+          args: ['magic']
+        }
+      }
+  */
+
+
+
   var command = options.command;
   var opts = options.opts;
   var cb = options.cb;
@@ -11,6 +48,7 @@ var createNewStream = function(options){
 
   window._globalSocket.emit(command, opts);
   window._globalSocket.on(UID, cb);
+
   return {
     emit: function(data){
       window._globalSocket.emit(UID, data);
