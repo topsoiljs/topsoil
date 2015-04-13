186a67bbaa4630fc854e5224a008f9043fb5a017
John Tan <tantai.john@gmail.com>
2015-04-13 13:18:01 -0700

    renamed streaming.js to streaming_client.js

---
+++ client/streaming/streaming_client.js
@@ -0,0 +1,19 @@
+var createNewStream = function(options){
+  socket, command, opts, cb
+  var socket = options.socket;
+  var command = options.command;
+  var opts = options.opts;
+  var cb = options.cb;
+  opts.initialData = options.initialData;
+
+  var UID = Math.random().toString(36) + Math.random().toString(36);
+  opts._uid = UID;
+
+  socket.emit(command, opts);
+  socket.on(UID, cb);
+  return {
+    emit: function(data){
+      socket.emit(UID, data);
+    }
+  }
+};
