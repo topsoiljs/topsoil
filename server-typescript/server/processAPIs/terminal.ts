f09a848b0633929f1c193aa15732f4c65a30c50d
John Tan <tantai.john@gmail.com>
2015-04-12 23:44:14 -0700

    migrated terminal api to streaming

--- server-typescript/server/processAPIs/terminal.ts
+++ server-typescript/server/processAPIs/terminal.ts
@@ -1,17 +1,10 @@
 /// <reference path="../../typings/node/node.d.ts"/>
-/// <reference path="../utility/utility.ts"/>
 var spawn = require('child_process').spawn;
-var utility = require('../utility/utility');
-
-interface listenerFunc {
-    (data:string) : any;
-}
-
+var createSpawnStreamF = require('../streaming/streaming');
 var terminalAPI = <any> {};
 
-terminalAPI.run = function(socket) {
-  return function(opts){   
-     utility.makeProcess(socket, opts.cmd, opts)
-  };  
-}
+terminalAPI.run = function(opts) {
+  return createSpawnStreamF(opts.cmd, opts.args, opts.opts);
+};
+
 module.exports = terminalAPI;
