9c708bffcd1487f09d432806d9a123b2b98f0a28
John Tan <tantai.john@gmail.com>
2015-04-14 22:10:16 -0700

    implemented repl stream

--- server-typescript/server/processAPIs/repl.ts
+++ server-typescript/server/processAPIs/repl.ts
@@ -5,12 +5,49 @@ var createGenericStreamFunc = streaming.createGenericStream;
 var createSpawnStreamFunc = streaming.createSpawnStream;
 var createOutSocket = streaming.createOutStream;
 var createInSocket = streaming.createInStream;
+var createDuplexStream = streaming.createDuplexStream
+var replAPI = <any> {};
 
-var server = require('socket.io')(8001);
-server.on('connection', function(socket){
-  var socketIn = createInSocket(socket, 'repl');
-  var socketOut = createOutSocket(socket, 'repl');
-})
+replAPI.start = function(options){
+  var inStream = createGenericStreamFunc(function(chunk, enc, cb){
+    cb(null, String(chunk));
+  })
+  var outStream = createGenericStreamFunc(function(chunk, enc, cb){
+    cb(null, chunk);
+  })
+  options.input = inStream;
+  options.output = process.stdout;
+  repl.start(options);
+  return createDuplexStream(inStream, outStream);
+}
+
+// var server = require('socket.io')(8001);
+// server.on('connection', function(socket){
+//   var stream;
+//   socket.on('startrepl', function(opts){
+//     opts = opts || {};
+//     var inStream = createInSocket(socket, 'repl');
+//     var outStream = createInSocket(socket, 'repl');
+//     stream = replAPI.start(opts)
+//     inStream.pipe(stream).pipe(outStream);
+//   })
+// })
 
 
-var client = require('socket.io-client')(8001);
+// var client = require('socket.io-client')('http://localhost:8001');
+
+// client.emit('startrepl', {})
+
+// client.emit('repl', {payload:'console.log("test")\n'})
+
+// client.on('repl', function(d){
+//   process.stdout.write(d.data);
+// })
+
+// process.stdin.on('data', function(d){
+//   client.emit('repl', {payload: String(d)})
+// })
+
+
+
+module.exports = replAPI;
