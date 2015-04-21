f6fe846517faed848f5339f4dbc36443efdfaebb
John Tan <tantai.john@gmail.com>
2015-04-20 22:00:11 -0700

    new subView component

---
+++ client/views/subViews.jsx
@@ -0,0 +1,19 @@
+var SubViews = React.createClass({
+  render: function(){
+    var nodes = [];
+    // nodes.push(
+    //   <div>branch: dev | changes: 3 lines</div>
+    // )
+    for(var i=0;i<8;i++){
+      console.log(nodes);
+      nodes.push(
+        <div className="subview">branch: dev | changes: 3 lines</div>
+      )
+    }
+    return (
+      <div className="subviews">{nodes}</div>
+    )
+  }
+})
+
+module.exports = SubViews;
