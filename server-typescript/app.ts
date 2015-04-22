8306af34c2fd3f4e766e94fbd21475958f6927f0
John Tan <tantai.john@gmail.com>
2015-04-22 16:50:10 -0700

    added nconf and new gulp tasks

--- server-typescript/app.ts
+++ server-typescript/app.ts
@@ -4,17 +4,21 @@
 var Hapi = require("hapi");
 var processManager = require('./server/processManager');
 var stateRoutes = require('./server/stateAPI/stateRoutes');
+var nconf = require('nconf');
+nconf.argv().env().file({file: './default_config.json'});
+// Global configuration
+global.nconf = nconf;
+
 //// Create a server with a host and port
 var server = new Hapi.Server();
 var setupSocketAPI = require('./server/processManager');
 
 server.connection({
     host: 'localhost',
-    port: 8001
+    port: nconf.get('port')
 });
 
 var io = require('socket.io')(server.listener);
-
 
 setupSocketAPI(io);
 stateRoutes(server);
