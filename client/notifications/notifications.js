230e055bc020f0c7d86b2c3aae441937ed8683eb
John Tan <tantai.john@gmail.com>
2015-04-25 14:21:18 -0700

    added notifications

--- client/notifications/notifications.js
+++ client/notifications/notifications.js
@@ -1,5 +1,12 @@
-var createNotification = function(title, options){
-  return new Notification(title, options);
+var createNotification = function(title, options, noclose){
+  var notification = new Notification(title, options);
+  // Autoclose
+  if(!noclose){
+    setTimeout(function(){
+      notification.close();
+    }, 5000)
+  };
+  return notification;
 };
 var notifications = {
   createNotification: createNotification
